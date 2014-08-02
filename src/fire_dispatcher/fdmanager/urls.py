3d1c401768ffddd95f1ffd7b5ee06f6fee102bf9
Manuel Peuster <manuel@peuster.de>
2014-08-02 10:38:03 +0200

    Removed dispatcher project.

--- src/fire_dispatcher/fdmanager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from fdmanager import views
-
-urlpatterns = patterns('',
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-)
--- src/fire_dispatcher/fdmanager/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-import logging
-
-
-def index(request):
-    logging.debug("Hello World")
-    return render(request, 'fdmanager/index.html', None)
