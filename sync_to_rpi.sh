cb458af8c2192339ec80fe9f9c4b1530e2a99c34
Manuel Peuster <manuel@peuster.de>
2014-08-08 17:33:05 +0200

    Removed old sync script

--- sync_to_rpi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-echo "========================= Starting PUSH to Raspberry PI  =========================="
-rsync --stats --delete -auvze 'ssh' /Users/manuel/Projects/open-fire-pager/src/ pi:/home/manuel/open-fire-pager/
-echo "=========================== Sync to Raspberry PI done. ============================"
