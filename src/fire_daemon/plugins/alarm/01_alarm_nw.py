bbf149d22451c06074beb0c0228843a03473f0cf
Manuel Peuster <manuel@peuster.de>
2014-08-08 16:47:02 +0200

    Added simple alarm and alive messages, for Alarm Dispatcher of FF Nieder-Werbe.

--- src/fire_daemon/plugins/alarm/01_alarm_nw.py
+++ src/fire_daemon/plugins/alarm/01_alarm_nw.py
@@ -3,12 +3,24 @@
 Alarm script to trigger SMS alarm: FF Nieder-Werbe.
 """
 import sys
+import requests
+
 
 ZVEI_CODES = ["51372"]
 
 
 def main():
     print "01_alarm_nw.py"
+    # load alarm url
+    f = open("data_alarmurl.txt", "r")
+    aurl = f.read()
+    f.close()
+    aurl = aurl.strip()
+    print "Alarm URL: %s" % aurl
+    print "Sending request..."
+    r = requests.get(aurl)
+    print "... done."
+    print "Result: Code: %s Body: %s" % (r.status_code, r.text)
 
 
 if __name__ == '__main__':
--- src/fire_daemon/plugins/wakeup/01_alivemessage.py
+++ src/fire_daemon/plugins/wakeup/01_alivemessage.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
-print "Alive message wakeup script is executed."
+"""
+Awake script to trigger SMS alarm alive monitor: FF Nieder-Werbe.
+"""
+import sys
+import requests
+
+
+def main():
+    print "01_allivemessage.py"
+    # load alarm url
+    f = open("data_awakeurl.txt", "r")
+    aurl = f.read()
+    f.close()
+    aurl = aurl.strip()
+    print "Awake URL: %s" % aurl
+    print "Sending request..."
+    r = requests.get(aurl)
+    print "... done."
+    print "Result: Code: %s Body: %s" % (r.status_code, r.text)
+
+
+if __name__ == '__main__':
+    main()
